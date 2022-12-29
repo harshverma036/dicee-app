da8df059b320d5a629b3cac60a2c3aa405063851
harshverma036 <harshverma0362@gmail.com>
2022-12-29 14:48:54 +0530

    dice completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -15,7 +16,24 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int rightDiceNumber = 2;
+  int leftDiceNumber = 1;
+
+  void randomizeDice() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,16 +41,20 @@ class DicePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children:  [
           Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(13.0),
-                child: Image.asset("images/dice1.png"),
-              ),
+              child: TextButton(
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+                onPressed: () {
+                  randomizeDice();
+                },
+              )
           ),
           Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(13.0),
-                child: Image.asset("images/dice2.png"),
-              ),
+              child: TextButton(
+                  child: Image.asset("images/dice$rightDiceNumber.png"),
+                  onPressed: () {
+                    randomizeDice();
+                  }
+              )
           ),
         ],
       ),
